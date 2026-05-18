54216a816007830d4a6d4d60c787a788c881bb7c
jimdriscoll0525 <jim@meploans.com>
2026-05-17 21:27:05 -0400

    Create BookmarkletLink.tsx

---
+++ app/bookmarklet/BookmarkletLink.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function BookmarkletLink({ packed }: { packed: string }) {
+  return (
+    
+      href={packed}
+      className="inline-block rounded-md bg-black px-4 py-2 font-medium text-white"
+      onClick={(e) => e.preventDefault()}
+    >
+      + Capture to SocialScopeIQ
+    </a>
+  );
+}
